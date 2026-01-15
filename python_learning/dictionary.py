17e4852ba2062b7bfef22dcc69b95e7e2b09658d
Nilay Öztürk <174735648+NilayOzturk@users.noreply.github.com>
2026-01-15 17:45:50 +0300

    Made dictionary two sides

--- python_learning/dictionary.py
+++ python_learning/dictionary.py
@@ -1,25 +1,46 @@
 print("--- 📖 My Mini Dictionary ---")
-print("Type a word to learn its meaning (or 'q' to quit).\n")
+print("1. English -> Turkish 🇬🇧 🇹🇷")
+print("2. Turkish -> English 🇹🇷 🇬🇧")
 
-dictionary = {
+choice = input("Select Mode (1 or 2): ").strip()
+
+eng_to_tr = {
     "Apple": "Elma",
     "Book": "Kitap",
     "Computer": "Bilgisayar",
-    "Developer": "Yazılımcı",
-    "Bug": "Yazılım hatası (Böcek)"
+    "Mouse": "Fare",
+    "School": "Okul"
 }
 
+tr_to_eng = {
+    "Elma": "Apple",
+    "Kitap": "Book",
+    "Bilgisayar": "Computer",
+    "Fare": "Mouse",
+    "Okul": "School"
+}
+
+if choice == '2':
+    active_dict = tr_to_eng
+    prompt_text = "Bir Türkçe kelime yaz: "
+    flag = "🇹🇷 -> 🇬🇧"
+else:
+    active_dict = eng_to_tr
+    prompt_text = "Enter an English word: "
+    flag = "🇬🇧 -> 🇹🇷"
+
+print(f"\n--- Mode Activated: {flag} ---")
+print("Type 'q' to quit.\n")
+
+
 while True:
+    word = input(prompt_text).strip().title()
 
-    word = input("👉 Enter an English word: ").strip().title()
-
-    if word == 'Q' or word == 'Quit':
-        print("Goodbye! 👋")
+    if word == 'Q' or word == 'Quit' or word == 'Çıkış':
+        print("Bye / Hoşça kal! 👋")
         break
 
-    if word in dictionary:
-        meaning = dictionary[word]
-        print(f"\n🔎 {word} found!")
-        print(f"🇹🇷 Turkish: {meaning}\n")
+    if word in active_dict:
+        print(f"✅ {word} = {active_dict[word]}\n")
     else:
-        print(f"❌ '{word}' not found in database.\n")
+        print(f"❌ '{word}' not found in database.\n{'-'*20}\n❌ '{word}' kütüphanede bulunmadı.")
