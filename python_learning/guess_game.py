f4233e61469d7e019701cda7131fc5152bb91572
Nilay Öztürk <174735648+NilayOzturk@users.noreply.github.com>
2026-01-15 13:58:19 +0300

    Added some features

--- python_learning/guess_game.py
+++ python_learning/guess_game.py
@@ -1,15 +1,31 @@
 import random
-print("---Number Guessing Game---")
-print("I'm guessng a number between 1 to 100.")
+print("🎲---Number Guessing Game---🎲")
+print("I'm guessing a number between 1 to 100.")
 
 secret_number = random.randint(1, 100)
+attempts = 0
 
 while True:
-    guess = int(input("Your guess: "))
-    if guess < secret_number:
-        print("Go HIGHER!")
-    elif guess > secret_number:
-        print("Go  LOWER!")
-    else:
-        print("🎉 CONGRATULATIONS! You found it!")
-        break
+    try:
+        guess = int(input("Your guess: "))
+        attempts +=1
+        if guess < secret_number:
+            print("❌ Too low! Go HIGHER ⬆️!")
+        elif guess > secret_number:
+            print("❌ Too high! Go LOWER ⬇️")
+        else:
+            print(f"🎉 CONGRATULATIONS! You found it in {attempts} try!")
+
+            if attempts == 1:
+                print("🌟 Rank: INCREDIBLE! Are you a psychic?!")
+            elif attempts <= 5:
+                print("😎 Rank: Genius! Very smart strategy.")
+            elif attempts <= 10:
+                print("👍 Rank: Good job! Above average.")
+            else:
+                print("😅 Rank: You struggled a bit, but you made it!")
+            
+            break
+
+    except ValueError:
+        print("⚠️ Please enter a valid number only!")
